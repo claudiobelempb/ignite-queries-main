b7784ccabe1bb929f1c70025243363d7f15fc796
claudiobelempb <claudio.c.lima@hotmail.com>
2021-04-15 21:25:42 -0300

    ignite-queries-main

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from "typeorm";
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User | undefined> {
+    // Complete usando ORM
+    return this.repository.findOne(user_id, { relations: ["games"] });
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(
+      `SELECT * FROM users WHERE lower(first_name)=lower('${first_name}') AND lower(last_name)=lower('${last_name}')`
+    ); // Complete usando raw query
+  }
+}
